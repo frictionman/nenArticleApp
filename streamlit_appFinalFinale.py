99135aae5b5980ea21d3c0cb2aa3533a9831ce6e
frictionman <48338507+frictionman@users.noreply.github.com>
2023-09-03 12:20:38 -0400

    Update streamlit_appFinalFinale.py

--- streamlit_appFinalFinale.py
+++ streamlit_appFinalFinale.py
@@ -10,8 +10,8 @@ from PIL import Image
 import requests
 import replicate
 
-# Loading the model
 def load_llm(max_tokens, prompt_template):
+    # Using the correct model identifier from the article
     llm_model_identifier = 'a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5'
     
     llm = CTransformers(
@@ -25,6 +25,7 @@ def load_llm(max_tokens, prompt_template):
         llm=llm,
         prompt=PromptTemplate.from_template(prompt_template)
     )
+    
     return llm_chain
 
 def get_src_original_url(query):
@@ -36,6 +37,7 @@ def get_src_original_url(query):
         'query': query,
         'per_page': 1,
     }
+    
     response = requests.get(url, headers=headers, params=params)
     if response.status_code == 200:
         data = response.json()
@@ -60,55 +62,58 @@ def create_word_docx(user_input, paragraph, image_filename):
 
 def main():
     st.title("Article Generator App using Llama 2")
+    
     user_input = st.text_input("Please enter the idea/topic for the article you want to generate!")
     image_input = st.text_input("Please enter the topic for the image you want to fetch!")
-
-    if len(user_input) > 0 and len(image_input) > 0:
+    result = ""
+    image_url = ""
+    
+    if user_input and image_input:
         col1, col2, col3 = st.columns([1,2,1])
+
+        # Article Generation
         with col1:
             st.subheader("Generated Content by Llama 2")
-            prompt_template = f"You are a digital marketing and SEO expert and your task is to write an article on the given topic: {user_input}. The article must be under 800 words."
+            prompt_template = "You are a digital marketing and SEO expert and your task is to write article so write an article on the given topic: {user_input}. The article must be under 800 words. The article should be be lengthy."
             
-            result = ""  # Initialize the result variable
             try:
                 llm_chain = load_llm(max_tokens=800, prompt_template=prompt_template)
-                generator_output = llm_chain(user_input)
+                generator_output = replicate.run(llm_chain, input={"prompt": user_input})
                 result = next(generator_output, {}).get('content', "")
-                
-                if result:
-                    st.info("Your article has been been generated successfully!")
-                    st.write(result)
-                else:
-                    st.error("Your article couldn't be generated or is empty!")
-
+                st.write(result)
             except Exception as e:
                 st.error(f"An error occurred while generating the article: {e}")
 
+        # Image Fetching
         with col2:
             st.subheader("Fetched Image")
-            image_url = get_src_original_url(image_input)
-            if image_url:
-                save_image_from_url(image_url)
-                st.image(image_url)
-            else:
-                st.error("Couldn't fetch the image for the given topic.")
+            try:
+                image_url = get_src_original_url(image_input)
+                if image_url:
+                    save_image_from_url(image_url)
+                    st.image(image_url)
+            except Exception as e:
+                st.error(f"An error occurred while fetching the image: {e}")
 
+        # Word Document Generation
         with col3:
             st.subheader("Final Article to Download")
-            if result and image_url:
-                image_filename = "temp_image.jpg"
-                doc = create_word_docx(user_input, result, image_filename)
-                doc_buffer = io.BytesIO()
-                doc.save(doc_buffer)
-                doc_buffer.seek(0)
-                st.download_button(
-                    label='Download Word Document',
-                    data=doc_buffer,
-                    file_name='document.docx',
-                    mime='application/vnd.openxmlformats-officedocument.wordprocessingml.document'
-                )
-            else:
-                st.error("Couldn't generate the Word document. Ensure both the article and image are generated successfully.")
+            try:
+                if result and image_url:
+                    doc = create_word_docx(user_input, result, "temp_image.jpg")
+                    doc_buffer = io.BytesIO()
+                    doc.save(doc_buffer)
+                    doc_buffer.seek(0)
+                    st.download_button(
+                        label='Download Word Document',
+                        data=doc_buffer,
+                        file_name='document.docx',
+                        mime='application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+                    )
+                else:
+                    st.warning("Ensure both the article and image are generated successfully.")
+            except Exception as e:
+                st.error(f"Couldn't generate the Word document. {e}")
 
 if __name__ == "__main__":
     main()
